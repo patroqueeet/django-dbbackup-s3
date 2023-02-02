e5e1892968923a256660ddfd8de9d3c8013a02d7
Jirka Schaefer <jirka@tschitschereengreen.com>
2023-02-02 13:06:38 +0100

    bump v0.5 w/ compress

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(here, "README.md"), "r", encoding="utf-8") as ffile:
 setup(
     name="django-dbbackup-s3",
     python_requires=">=3.7.0",
-    version="0.4",
+    version="0.5",
     author="Jirka Schaefer",
     author_email="info@tschitschereengreen.com",
     packages=["storage_sync", "storage_sync.management.commands"],
