146c14883738dec054686d5f4b4e04a495b8c7ce
Jirka Schaefer <jirka@tschitschereengreen.com>
2024-11-14 14:50:30 +0100

    must use changed param name

--- tests/test_command.py
+++ tests/test_command.py
@@ -9,7 +9,7 @@ class S3BackupCommandTestCase(TestCase):
         call_command("s3backup")
         sync_mock.assert_called_with(
             compress=False,
-            dropbox_dir="dropbox-dest-dir/",
+            target_dir="dropbox-dest-dir/",
             s3_bucket="",
             s3_dir="s3-source-dir/",
             target_file_name="backup.tar.gz",
